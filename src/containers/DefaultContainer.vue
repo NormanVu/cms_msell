8fb129116675fef892b463fe65fd17c26675a77a
norman <norman@enclave.vn>
2019-12-03 16:09:10 +0700

    Add init vue admin template using CoreUI

---
+++ src/containers/DefaultContainer.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="app">
+    <AppHeader fixed>
+      <SidebarToggler class="d-lg-none" display="md" mobile />
+      <b-link class="navbar-brand" to="#">
+        mSell
+      </b-link>
+      <SidebarToggler class="d-md-down-none" display="lg" />
+      <b-navbar-nav class="ml-auto">
+        <DefaultHeaderDropdownAccnt/>
+      </b-navbar-nav>
+      
+    </AppHeader>
+    <div class="app-body">
+      <AppSidebar fixed>
+        <SidebarHeader/>
+        <SidebarForm/>
+        <SidebarNav :navItems="nav"></SidebarNav>
+        <SidebarFooter/>
+        <SidebarMinimizer/>
+      </AppSidebar>
+      <main class="main">
+        <Breadcrumb :list="list"/>
+        <div class="container-fluid">
+          <router-view></router-view>
+        </div>
+      </main>
+      <AppAside fixed>
+        <!--aside-->
+        <DefaultAside/>
+      </AppAside>
+    </div>
+    <TheFooter>
+      <!--footer-->
+      <div>Copyright &copy; 2019 <a href="www.maxis.com.my">Maxis</a>.<span class="ml-1"> All rights reserved.</span></div>
+    </TheFooter>
+  </div>
+</template>
+
+<script>
+import nav from '@/_nav'
+import { Header as AppHeader, SidebarToggler, Sidebar as AppSidebar, SidebarFooter, SidebarForm, SidebarHeader, SidebarMinimizer, SidebarNav, Aside as AppAside, AsideToggler, Footer as TheFooter, Breadcrumb } from '@coreui/vue'
+import DefaultAside from './DefaultAside'
+import DefaultHeaderDropdownAccnt from './DefaultHeaderDropdownAccnt'
+
+export default {
+  name: 'DefaultContainer',
+  components: {
+    AsideToggler,
+    AppHeader,
+    AppSidebar,
+    AppAside,
+    TheFooter,
+    Breadcrumb,
+    DefaultAside,
+    DefaultHeaderDropdownAccnt,
+    SidebarForm,
+    SidebarFooter,
+    SidebarToggler,
+    SidebarHeader,
+    SidebarNav,
+    SidebarMinimizer
+  },
+  data () {
+    return {
+      nav: nav.items
+    }
+  },
+  computed: {
+    name () {
+      return this.$route.name
+    },
+    list () {
+      return this.$route.matched.filter((route) => route.name || route.meta.label )
+    }
+  }
+}
+</script>
